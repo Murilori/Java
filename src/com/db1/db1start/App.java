c30ecd96bbba2f81a644c54bbe1c21db53f5ea81
Murilo <murilorgoo@gmail.com>
2019-11-11 17:06:15 -0300

    Commit de soma

--- src/com/db1/db1start/App.java
+++ src/com/db1/db1start/App.java
@@ -2,6 +2,8 @@ package com.db1.db1start;
 
 import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
         //palavras reservadas
@@ -14,6 +16,15 @@ public class App {
 
         metodo main é necessário para que o java consiga entender
          */
-        Integer numero;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        Integer numero1 =  scan.nextInt();
+
+        System.out.println("Digite outro numero: ");
+        Integer numero2 = scan.nextInt();
+
+        System.out.println(numero1 + numero2);
+
+        
     }
 }
