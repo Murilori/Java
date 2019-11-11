f0432630b8a5534d70d2780bf33d0dc151c50ef3
Murilo <murilorgoo@gmail.com>
2019-11-11 15:33:55 -0300

    commit dos arquivos de java

---
+++ src/com/db1/db1start/App.java
@@ -0,0 +1,17 @@
+package com.db1.db1start;
+
+public class App {
+    public static void main(String[] args){
+        //palavras reservadas
+        /*
+        package
+        public
+        static
+        void
+        class
+
+        metodo main é necessário para que o java consiga entender
+         */
+        System.out.println("Hello world!");
+    }
+}
